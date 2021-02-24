824bc251a9d3cb67b9ff873084954d0607c5a677
caleb15 <caleb@15five.com>
2021-02-24 13:34:50 -0800

    add more tests

--- test_foo.py
+++ test_foo.py
@@ -1,4 +1,6 @@
 import foo
+import json
+import pytest
 
 # This file uses pytest-mock library
 # If you don't want to use pytest-mock
@@ -17,9 +19,50 @@ def test(mocker):
     do_stuff()
 """
 
+# patch path examples
+
 def test_patch_library_function(mocker):
+    # the import: from random import randint
+    # because import is in foo we need to reference it with foo
     mocker.patch('foo.randint', return_value=0)
     assert foo.get_large_random_number() == 0
+
+def test_patch_func_from_another_file(mocker):
+    # Even tho "api_call" is in "call_api" file
+    # We still reference it via "foo.api_call"
+    # The import: from call_api import api_call
+    m = mocker.patch('foo.api_call')
+    foo.do_foo() == m
+
+def test_patch_json_dumps_bad(mocker):
+    # The import: import json
+    # usage: json.dumps()
+    # below would be incorrect because "dumps" is not a property of the "foo" module,
+    # it's a property of "json"
+    with pytest.raises(AttributeError):
+        m = mocker.patch('foo.dumps')
+
+
+def test_patch_json_dumps_stdlib(mocker):
+    # This technically works
+    # but it also mocks stdlib functions in this file!
+    m = mocker.patch('json.dumps')
+    foo.call_json_dumps() == m # works!
+    with pytest.raises(AssertionError):
+        assert json.dumps({'f':3}) == '{"f":3}'
+
+
+def test_patch_json_dumps(mocker):
+    m = mocker.patch('foo.json.dumps')
+    foo.call_json_dumps() == m
+
+def test_patch_object(mocker):
+    # same thing as just .patch except you can pass object in directly
+    # instead of doing .patch('foo.api_call')
+    mocker.patch.object(foo, 'api_call')
+    foo.do_foo()
+
+# other examples
 
 # unlike regular mocker pytest mocker can't be used with "with"
 # def test_count_calls(mocker):
@@ -34,16 +77,6 @@ def test_count_calls(mocker):
     # foo.get_large_random_number() # uncommenting this would correctly cause test to fail
     m.assert_called_once()
 
-def test_patch_foo(mocker):
-    mocker.patch('foo.api_call')
-    foo.do_foo()
-
 def test_patch_foo_with_val(mocker):
     mocker.patch('foo.api_call', return_value=3)
     assert foo.do_foo() == 3
-
-def test_patch_object(mocker):
-    # same thing as just .patch except you can pass object in directly
-    # instead of doing .patch('foo.api_call')
-    mocker.patch.object(foo, 'api_call')
-    foo.do_foo()
