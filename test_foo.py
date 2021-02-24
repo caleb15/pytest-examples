d2a0293d41e8179952b61005b84b2fdd14afdfab
caleb15 <caleb@15five.com>
2021-02-24 13:35:07 -0800

    blackify

--- test_foo.py
+++ test_foo.py
@@ -21,18 +21,21 @@ def test(mocker):
 
 # patch path examples
 
+
 def test_patch_library_function(mocker):
     # the import: from random import randint
     # because import is in foo we need to reference it with foo
-    mocker.patch('foo.randint', return_value=0)
+    mocker.patch("foo.randint", return_value=0)
     assert foo.get_large_random_number() == 0
+
 
 def test_patch_func_from_another_file(mocker):
     # Even tho "api_call" is in "call_api" file
     # We still reference it via "foo.api_call"
     # The import: from call_api import api_call
-    m = mocker.patch('foo.api_call')
+    m = mocker.patch("foo.api_call")
     foo.do_foo() == m
+
 
 def test_patch_json_dumps_bad(mocker):
     # The import: import json
@@ -40,27 +43,29 @@ def test_patch_json_dumps_bad(mocker):
     # below would be incorrect because "dumps" is not a property of the "foo" module,
     # it's a property of "json"
     with pytest.raises(AttributeError):
-        m = mocker.patch('foo.dumps')
+        m = mocker.patch("foo.dumps")
 
 
 def test_patch_json_dumps_stdlib(mocker):
     # This technically works
     # but it also mocks stdlib functions in this file!
-    m = mocker.patch('json.dumps')
-    foo.call_json_dumps() == m # works!
+    m = mocker.patch("json.dumps")
+    foo.call_json_dumps() == m  # works!
     with pytest.raises(AssertionError):
-        assert json.dumps({'f':3}) == '{"f":3}'
+        assert json.dumps({"f": 3}) == '{"f":3}'
 
 
 def test_patch_json_dumps(mocker):
-    m = mocker.patch('foo.json.dumps')
+    m = mocker.patch("foo.json.dumps")
     foo.call_json_dumps() == m
+
 
 def test_patch_object(mocker):
     # same thing as just .patch except you can pass object in directly
     # instead of doing .patch('foo.api_call')
-    mocker.patch.object(foo, 'api_call')
+    mocker.patch.object(foo, "api_call")
     foo.do_foo()
+
 
 # other examples
 
@@ -72,11 +77,12 @@ def test_patch_object(mocker):
 
 # instead you should just get ref directly
 def test_count_calls(mocker):
-    m=mocker.patch('foo.randint')
+    m = mocker.patch("foo.randint")
     foo.get_large_random_number()
     # foo.get_large_random_number() # uncommenting this would correctly cause test to fail
     m.assert_called_once()
 
+
 def test_patch_foo_with_val(mocker):
-    mocker.patch('foo.api_call', return_value=3)
+    mocker.patch("foo.api_call", return_value=3)
     assert foo.do_foo() == 3
